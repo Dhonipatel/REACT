bd15a6183f9dd0c340463b3fdd538355afcef2ae
Dhoni Patel <iamdhonni@gmail.com>
2026-06-20 12:51:19 +0530

    change

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,7 +1,19 @@
 import"./App.css"
 
+function Title() {
+  return <h1>I am Title</h1>
+}
+
 function App() {
-  return  <button>Hello World</button>;
+  return (
+      <div>
+        <h1>This is my app component</h1>
+        <p>inside app component we have :</p>
+    <Title/>
+  </div> 
+
+  );
 }
 
 export default App
+ 
